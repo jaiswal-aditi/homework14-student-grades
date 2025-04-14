c7fa3a85728185664d4a2fb402ab75e9f774044e
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-04-13 18:48:02 -1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 /*
-Design a C++ applica.on to collect, validate, and display academic details for mul.ple students.
+Design a C++ application to collect, validate, and display academic details for multiple students.
 For each student, the user must input their first name, last name, class, roll number, and marks.
 The system should validate all inputs (ensuring correct types and meaningful values), calculate
-the student’s grade based on their marks, and then display each student’s complete informa.on.
-The system must support mul.ple students using a fixed-size array.
+the student’s grade based on their marks, and then display each student’s complete information.
+The system must support multiple students using a fixed-size array.
 
 
   See PDF for sample input/output
