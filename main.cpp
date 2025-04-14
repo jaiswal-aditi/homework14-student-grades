7644cabab2649ce2b6b31c4ef73012733e1025ca
Aditi Jaiswal <66053145+jaiswal-aditi@users.noreply.github.com>
2025-04-13 18:46:47 -1000

    Update and rename person.cpp to main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/*
+Design a C++ applica.on to collect, validate, and display academic details for mul.ple students.
+For each student, the user must input their first name, last name, class, roll number, and marks.
+The system should validate all inputs (ensuring correct types and meaningful values), calculate
+the student’s grade based on their marks, and then display each student’s complete informa.on.
+The system must support mul.ple students using a fixed-size array.
+
+
+  See PDF for sample input/output
+
+*/
